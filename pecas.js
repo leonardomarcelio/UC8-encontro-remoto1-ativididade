fc6ac021338461820fed5e5ea6259f8510f03ad7
Leonardo Rodrigues <leonardomarcelio130@gmail.com>
2021-10-07 14:33:31 -0300

    commit encontro remoto1 UC8

---
+++ pecas.js
@@ -0,0 +1,20 @@
+let peso = 110;
+
+if(peso >= 100){
+ console.log("A peça possui um peso adequado, pode ser cadastrada")}
+ else{
+    console.log(" A peça não possui o peso mínimo")}
+    
+let listaDePecas = ["vela", "disco de freio",]
+
+if (listaDePecas.length < 10) { 
+    console.log(" A lista possui espaço para mais peças")
+    
+} else {
+    console.log (" A lista está lotado, não é possível incluir novas peças")
+   }
+let nomePeca = listaDePecas [0]
+   if (nomePeca.length > 3) { console.log ("o nome da peça é válido, pode seguir com o cadastro")
+       
+   } else {
+       ("o nome da peça é invalido, o nome deve ter mais que três caracteres") }
