4f03cee36c8ae464ccfcb328f7f278b0817f61ba
Leonardo Rodrigues <leonardomarcelio130@gmail.com>
2021-10-08 22:50:46 -0300

    correção

--- pecas.js
+++ pecas.js
@@ -1,11 +1,11 @@
 let peso = 110;
 
-if(peso >= 100){
+if(peso > 100){
  console.log("A peça possui um peso adequado, pode ser cadastrada")}
  else{
     console.log(" A peça não possui o peso mínimo")}
     
-let listaDePecas = ["vela", "disco de freio",]
+let listaDePecas = ["vela", "disco de freio", "fio"]
 
 if (listaDePecas.length < 10) { 
     console.log(" A lista possui espaço para mais peças")
@@ -13,8 +13,8 @@ if (listaDePecas.length < 10) {
 } else {
     console.log (" A lista está lotado, não é possível incluir novas peças")
    }
-let nomePeca = listaDePecas [0]
+let nomePeca = listaDePecas [2]
    if (nomePeca.length > 3) { console.log ("o nome da peça é válido, pode seguir com o cadastro")
        
    } else {
-       ("o nome da peça é invalido, o nome deve ter mais que três caracteres") }
+      console.log ("o nome da peça é invalido, o nome deve ter mais que três caracteres") }
